278a89d020d523fa59ac9082ac4194809e5efa1e
= <yohan.ariasvasquez@gsquad.io>
2020-06-03 00:09:38 -0500

    functionality to upload the csv file to convert

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request, render_template
 from .proofpoint_parser import ProofPointParser
 import io
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def return_csv():
-    file = open('sample_file.csv', 'r')
-    parser = ProofPointParser(file)
-    output_file = parser.get_csv()
-    mem = io.BytesIO()
-    mem.write(output_file.getvalue().encode('utf-8'))
-    mem.seek(0)
-    return send_file(mem, as_attachment=True, attachment_filename='output.csv', mimetype='text/csv')
+    if request.method == 'POST':
+        file = request.files['file']
+        # print(dummy_file)
+        # file = open('sample_file.csv', 'r')
+        parser = ProofPointParser(file)
+        output_file = parser.get_csv()
+        mem = io.BytesIO()
+        mem.write(output_file.getvalue().encode('utf-8'))
+        mem.seek(0)
+        return send_file(mem, as_attachment=True, attachment_filename='output.csv', mimetype='text/csv')
+    return render_template('upload_form.html')
